935ce07e74f3dba86aaa37c6bef5fecc236d4542
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-05-18 02:05:22 -0300

    Using Image e ImageBackground t styling the project

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
-import Navigator from './routes/drawer';
+import Navigator from './routes/Drawer';
 
 const getFont = () => Font.loadAsync({
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
--- src/screens/ReviewDetails.js
+++ src/screens/ReviewDetails.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 import Card from '../../shared/Card';
 
-import { globalStyles } from '../../styles/global';
+import { globalStyles, images } from '../../styles/global';
 
 export default function ReviewDetails({ route, navigation }) {
+
+    const { title, body, rating } = route.params;
 
     return (
         <View style={globalStyles.container}>
             <Card>
-                <Text>{route.params.title}</Text>
-                <Text>{route.params.body}</Text>
-                <Text>{route.params.rating}</Text>
+                <Text>{title}</Text>
+                <Text>{body}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+});
