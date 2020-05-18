91eeb051f1dafb9acae540bee03828959374977e
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-05-18 01:13:24 -0300

    Making a Custom Card Component to reviews

--- src/screens/ReviewDetails.js
+++ src/screens/ReviewDetails.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
+import Card from '../../shared/Card';
+
 import { globalStyles } from '../../styles/global';
 
 export default function ReviewDetails({ route, navigation }) {
 
-    console.log(route)
     return (
         <View style={globalStyles.container}>
-            <Text>{route.params.title}</Text>
-            <Text>{route.params.body}</Text>
-            <Text>{route.params.rating}</Text>
+            <Card>
+                <Text>{route.params.title}</Text>
+                <Text>{route.params.body}</Text>
+                <Text>{route.params.rating}</Text>
+            </Card>
         </View>
     )
 }
